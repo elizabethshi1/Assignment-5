096509ac40fc62a5f948e9aa381fc1c1403d8725
Elizabeth Shi <elizabeth.shi@gmail.com>
2021-12-16 04:02:36 -0500

    Updates
    Added Hospital data table

--- Streamlit.py
+++ Streamlit.py
@@ -8,3 +8,11 @@ Created on Thu Dec 16 01:25:40 2021
 import streamlit as st
 import pandas as pd
 import numpy as np
+
+st.title("Elizabeth's version of Streamlit")
+
+df_Hospital = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_DataSci_507/main/Deployment_Streamlit/hospital_info.csv')
+df_Outpatient = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_DataSci_507/main/Deployment_Streamlit/outpatient_2015.csv')
+df_Inpatient =pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_DataSci_507/main/Deployment_Streamlit/inpatient_2015.csv')
+
+st.dataframe(df_Hospital)
